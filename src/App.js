711605066b577dc68826e606f8f969ac60585e1c
Wren <wrenparker@gmail.com>
2020-01-02 12:12:47 -0500

    changed css units to match (fr, px, em)

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import './css/App.css';
 import HomePage from "./Components/HomePage/HomePage";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 
 function App() {
   return (
+  <Router>
     <div>
         <HomePage/>
     </div>
+  </Router>
   );
 }
 
