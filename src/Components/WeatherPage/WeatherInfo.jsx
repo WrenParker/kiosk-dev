e6d9a203a9a2462c42f3a8db5a9f5fc2e3757de7
Wren Parker <wlp0003@mix.wvu.edu>
2020-01-10 14:10:51 -0500

    added water Info api

--- src/Components/WeatherPage/WeatherInfo.jsx
+++ src/Components/WeatherPage/WeatherInfo.jsx
@@ -20,8 +20,11 @@ class WeatherInfo extends React.Component {
         this.weatherKey = apiKeys.default.keys.weather;
         this.state = {
           weatherData: null,
-          isLoaded: false,
-          hasLoaded: false
+          waterData: null,
+          weatherIsLoaded: false,
+          weatherHasLoaded: false,
+          waterIsLoaded: false,
+          waterHasLoaded: false,
         };
         this.getWeatherData();
 
@@ -33,13 +36,16 @@ class WeatherInfo extends React.Component {
           () => this.tick(), 600000
       );
       this.getWeatherData();
+      this.getWaterData();
     }
 
     tick() {
 
         this.setState({
-            isLoaded: false,
-            weatherData: this.getWeatherData()
+            weatherIsLoaded: false,
+            waterIsLoaded: false,
+            weatherData: this.getWeatherData(),
+            waterData: this.getWaterData(),
         });
     }
 
@@ -58,8 +64,25 @@ class WeatherInfo extends React.Component {
         if(this._isMounted) {
           this.setState({
             weatherData: data,
-            isLoaded: true,
-            hasLoaded: true
+            weatherIsLoaded: true,
+            weatherHasLoaded: true
+          });
+        }
+      })
+    }
+
+    getWaterData() {
+      const waterURL =
+      `https://waterservices.usgs.gov/nwis/dv/?format=json&sites=03185400&siteStatus=all`;
+
+      fetch(waterURL)
+      .then(res=>res.json())
+      .then(data=> {
+        if(this._isMounted) {
+          this.setState({
+            waterData: data,
+            waterIsLoaded: true,
+            waterHasLoaded: true
           });
         }
       })
@@ -67,31 +90,67 @@ class WeatherInfo extends React.Component {
 
     currentTemperature() {
       let currentTemperature = 0;
-      if(this.state.isLoaded) {
+      if(this.state.weatherIsLoaded) {
         currentTemperature = Math.round(this.state.weatherData.main.temp);
         console.log(this.state.weatherData);
+        console.log(this.state.waterData.value.timeSeries[0].values[0].value[0].value);
       }
       return currentTemperature;
     }
 
+    currentFeelsLike() {
+      let currentFeelsLike = 0;
+      if(this.state.weatherIsLoaded) {
+        currentFeelsLike = Math.round(this.state.weatherData.main.feels_like);
+      }
+      return currentFeelsLike;
+    }
+
     currentDescription() {
       let currentDescription = 0;
-      if(this.state.isLoaded) {
+      if(this.state.weatherIsLoaded) {
         currentDescription = this.state.weatherData.weather[0].description;
       }
       return currentDescription;
     }
 
+    currentWindSpeed() {
+      let currentWindSpeed = 0;
+      if(this.state.weatherIsLoaded) {
+        currentWindSpeed = Math.round(this.state.weatherData.wind.speed);
+      }
+      return currentWindSpeed;
+    }
+
+    currentWaterTemperature() {
+      let currentWaterTemperature = 0;
+      if(this.state.waterIsLoaded) {
+        currentWaterTemperature = Math.round(((this.state.waterData.value.timeSeries[0].values[0].value[0].value) * (9/5))+32 );
+      }
+      return currentWaterTemperature;
+    }
+
     render () {
-        const {hasLoaded, weatherData} = this.state;
-         if(hasLoaded) { return (
+        const {weatherHasLoaded, waterHasLoaded, weatherData} = this.state;
+         if(weatherHasLoaded && waterHasLoaded) { return (
           <div className="Weather-Info-Wrapper">
             <div className="Outside-Basic-Wrapper">
               <div className="Outside-Basic-Temperature">
-                {this.currentTemperature() + "°" }
+                {this.currentTemperature() + "°"}
+              </div>
+              <div className="Outside-Basic-Feels-Like">
+                {"Feels Like " + this.currentFeelsLike() + "°"}
               </div>
               <div className="Outside-Basic-Description">
                 {this.currentDescription()}
+              </div>
+            </div>
+            <div className="Outside-Advanced-Wrapper">
+              <div className="Outside-Wind-Speed">
+                {"Wind Speed: " + this.currentWindSpeed() + "mph"}
+              </div>
+              <div className="Outside-Water-Temperature">
+                {"Water Temperature: " + this.currentWaterTemperature() + "°"}
               </div>
             </div>
           </div>
