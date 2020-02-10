2c65647e613ab6ea8f9883d4e141f641fd28e368
Wren <wrenparker@gmail.com>
2020-02-10 14:28:05 -0500

    attempt to fix schedule. issue not resolved, may be on google's end

--- src/Components/SchedulePage/Schedule.jsx
+++ src/Components/SchedulePage/Schedule.jsx
@@ -7,7 +7,7 @@ class Schedule extends React.Component {
   render () {
     return (
       <div className="Calendar">
-        <webview src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23616161&amp;ctz=America%2FNew_York&amp;src=d2xwMDAwM0BtaXgud3Z1LmVkdQ&amp;color=%2322AA99&amp;showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=WEEK" className="Calendar-Style" frameBorder="0" scrolling="no"></webview>
+        <webview src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;showTitle=0&amp;showNav=0&amp;showDate=1&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=1&amp;mode=WEEK&amp;src=d2xwMDAwM0BtaXgud3Z1LmVkdQ&amp;color=%23039BE5" className="Calendar-Style" frameborder="0" scrolling="no"></webview>
       </div>
     )
   }
