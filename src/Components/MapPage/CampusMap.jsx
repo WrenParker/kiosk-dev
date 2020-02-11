30d54218d0778e97b557356d1d78f2bff096ae74
Wren <wrenparker@gmail.com>
2020-02-11 10:37:18 -0500

    changed bounds of map, zoom, got map to work in build

--- src/Components/MapPage/CampusMap.jsx
+++ src/Components/MapPage/CampusMap.jsx
@@ -1,37 +1,36 @@
-import React, { Component } from 'react'
-import ReactDOM from "react-dom";
-import '../../css/Map.css';
-import { Icon } from "leaflet";
+import React from 'react';
+import L from 'leaflet';
+import '../../css/leaflet.css';
 
-import { Map, TileLayer, Rectangle } from 'react-leaflet'
-//38.0943,-81.0829
-//38.0844,-81.0755
-const outer = [
-  [38.0943, -81.0829],
-  [38.0844, -81.0755],
-]
-const inner = [
-  [38.0943, -81.0829],
-  [38.0844, -81.0755],
-]
+class CampusMap extends React.Component {
+  componentDidMount() {
+    let southWest = L.latLng(38.0823,-81.0846)
+    let northEast = L.latLng(38.0945, -81.0724)
+    let bounds = L.latLngBounds(southWest, northEast);
 
-type State = {
-  bounds: Array<[number, number]>,
-}
-
-export default class BoundsExample extends Component<{}, State> {
-  state = {
-    bounds: outer,
+    this.map = L.map('map', {
+      maxBounds: bounds,
+      center: [38.08721,-81.07914],
+      zoom: 15,
+      maxZoom: 17,
+      minZoom: 13,
+      layers: [
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }),
+      ]
+    });
   }
 
   render() {
     return (
-        <Map  className="Map-Container" bounds={this.state.bounds}>
-          <TileLayer
-            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </Map>
-    )
-  }
+    <div>
+      <div id="map"></div>
+    </div>
+  )}
 }
+
+export default CampusMap;
+
+//38.0943,-81.0829
+//38.0844,-81.0755
