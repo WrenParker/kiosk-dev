c64e703da0ba5515f23f4d91ff5ec267b9a1b2f1
Wren <wrenparker@gmail.com>
2020-03-09 11:55:46 -0400

    added buttons and css to download page

--- src/Components/QRPage/QRInterface.jsx
+++ src/Components/QRPage/QRInterface.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from "react-dom";
 import '../../css/QR.css';
+import {
+  HashRouter as Router,
+  Link
+} from "react-router-dom";
+import {Button} from "react-bootstrap";
 
 class QRInterface extends React.Component {
   contructor(props) {
@@ -10,11 +15,22 @@ class QRInterface extends React.Component {
 
   render () {
     return (
-      <div className="QR-Container">
-        <div className="QR-Image">
-
+      <Router>
+        <div className="QR-Container">
+          <Link className="Direct-Download">
+            <Button className="Button">
+              QR Link
+            </Button>
+          </Link>
+          <Link className="QR-Download">
+            <Button className="Button">
+              Download through App
+            </Button>
+          </Link>
+          <div className="QR-Image">
+          </div>
         </div>
-      </div>
+      </Router>
     )
   }
 }
