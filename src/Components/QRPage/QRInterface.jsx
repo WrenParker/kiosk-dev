77e4630e0b394e3a115fdf5ed0f3dbdd63554aab
Wren <wrenparker@gmail.com>
2020-03-09 16:42:26 -0400

    saving changes to publish

--- src/Components/QRPage/QRInterface.jsx
+++ src/Components/QRPage/QRInterface.jsx
@@ -6,28 +6,50 @@ import {
   Link
 } from "react-router-dom";
 import {Button} from "react-bootstrap";
+const QR = require('qrcode')
 
 class QRInterface extends React.Component {
-  contructor(props) {
+  constructor(props) {
+    super(props);
 
-
+    //this.data = props.data;
   }
+
+  // flashQR() {
+  //   const stringList = [];
+  //   let qrString = this.data;
+  //   let size = 25;
+  //   let length = Math.ceil((qrString.length)/size);
+  //   console.log(length);
+  //   for (let i=0; i<length; i++) {
+  //       if(i===0) {
+  //           stringList.push(qrString.slice(0,size));
+  //       }
+  //       else if(i === length) {
+  //           stringList.push(qrString.slice(size*i,this.data.length));
+  //       }
+  //       else {
+  //           stringList.push(qrString.slice(size*i,(size*(i+1))-1));
+  //       }
+  //   }
+  //   console.log(stringList);
+  // }
 
   render () {
     return (
       <Router>
         <div className="QR-Container">
-          <Link className="Direct-Download">
+          <div className="Direct-Download">
             <Button className="Button">
               QR Link
             </Button>
-          </Link>
-          <Link className="QR-Download">
+          </div>
+          <div className="QR-Download">
             <Button className="Button">
               Download through App
             </Button>
-          </Link>
-          <div className="QR-Image">
+          </div>
+          <div id="QR-Img" className="QR-Image">
           </div>
         </div>
       </Router>
